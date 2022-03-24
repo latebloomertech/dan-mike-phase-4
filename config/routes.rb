7f03759fe1c4c1e4c8124a7096777533df18b710
Dan Baechtold <latebloomertech@gmail.com>
2022-03-24 18:47:57 -0400

    added basic models and migrations and set model relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :recipe_ingredients
+  resources :ingredients
+  resources :recipes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get '/hello', to: 'application#hello_world'
 end
